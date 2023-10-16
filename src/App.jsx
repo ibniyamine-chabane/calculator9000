94db9ecfdf57379744f16685b67c207fd9ca3f4d
ibniyamine-chabane <ibni-yamine.chabane@laplateforme.io>
2023-10-16 10:56:05 +0200

    first commit + job01 Done

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Title from './components/Title'
+
+function App() {
+
+  return (
+    <>
+      <Title text="Calculator9000 "/>
+    </>
+  )
+}
+
+export default App
